433f0e9894c4c51b85c264d4c7a2385d3e0199b1
coldthaw <israel1337b@gmail.com>
2022-05-17 20:09:52 -0500

    email

--- components/LinksBar.js
+++ components/LinksBar.js
@@ -6,7 +6,7 @@ import { FaEnvelope } from 'react-icons/fa';
 const LinksBar = () => {
   return (
     <div className={styles.container}>
-      <a className={styles.link} href="#contact"><FaEnvelope className={styles.icon} /><span>israel@dev.israelb.xyz</span></a>
+      <a className={styles.link} href="#contact"><FaEnvelope className={styles.icon} /><span>israel@israelb.xyz</span></a>
       <div className={styles.socials}>
         <a className={styles.link} href="https://www.linkedin.com/in/israel-bonilla/" target="_blank" rel="noreferrer"><FaLinkedin className={styles.icon} /><span>LinkedIn</span></a>
         <a className={styles.link} href="https://github.com/israel-bonilla" target="_blank" rel="noreferrer"><FaGithub className={styles.icon} /><span>GitHub</span></a>
